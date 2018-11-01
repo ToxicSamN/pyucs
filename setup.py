f934477491e25f12361b8bb3918d226312b4e903
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-11-01 10:10:10 -0600

    new pyucs version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyucs-samn',
-    version='1.0.0',
+    version='1.1.0',
     description='Customized UCS Python Module',
 	long_description=long_description,
     license='Apache',
